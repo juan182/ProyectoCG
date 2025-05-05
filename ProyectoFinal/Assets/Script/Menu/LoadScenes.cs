bd2ae6a7fe5ee4356811e6ffeedfb23bb99af643
juan182 <60664888+juan182@users.noreply.github.com>
2025-05-05 18:21:30 -0500

    Menu inicio

---
+++ ProyectoFinal/Assets/Script/Menu/LoadScenes.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScenes : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    //Cargar scene 
+    public void LoadScene(string nameScene)
+    {
+        if (Timer.Instance != null)
+        {
+            Timer.Instance.TimerStart();
+        }
+        SceneManager.LoadScene(nameScene);
+
+    }
+
+    public void CloseGame()
+    {
+        Application.Quit();
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
+}
