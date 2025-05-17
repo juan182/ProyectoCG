840ffa31497750ce729ce4044333a65f8bb1e48d
YenMur <yennifer.murillo@uao.edu.co>
2025-05-16 20:51:49 -0500

    intento de animacion

--- ProyectoFinal/Assets/Script/Player/Movimiento/Nivel2Player.cs
+++ ProyectoFinal/Assets/Script/Player/Movimiento/Nivel2Player.cs
@@ -4,23 +4,6 @@ using UnityEngine;
 
 public class Nivel2Player : MonoBehaviour
 {
-    private Animator playerAnimator;
-
-    private void Start()
-    {
-        // Buscar el Animator del jugador hijo de la canoa
-        playerAnimator = GetComponentInChildren<Animator>();
-    }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("Shark"))
-        {
-            if (playerAnimator != null)
-            {
-                playerAnimator.SetTrigger("panic");
-            }
-        }
-    }
+ //Aqui va animacion
 
 }
